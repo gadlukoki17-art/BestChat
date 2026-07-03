112f9c96fbd8a83908f23224597fdc89aca27ce9
Gad lukoki <gadlukoki17@gmail.com>
2026-07-03 01:35:15 +0100

    Setting up the layout and logic for login.js

--- assets/js/login.js
+++ assets/js/login.js
@@ -8,3 +8,66 @@ const eyeClosed = document.getElementById("eye-closed");
 const remember = document.getElementById("remember");
 const loginBtn = document.getElementById("loginbtn");
 const spinner = document.getElementById("spinner");
+const hidebtn = document.getElementById("hidebtn");
+const btnText = document.getElementById("btn-text");
+
+function setLoading(isloading) {
+    if(isloading) {
+        loginBtn.disabled = true;
+        spinner.classList.remove("hidden");
+        btnText.textContent = "Logging in ...";
+
+    } else {
+        loginBtn.disabled = false;
+        spinner.classList.add("hidden");
+        btnText.textContent = "Login"
+    }
+}
+
+loginForm.addEventListener("submit", async (event) => {
+    const email = emailInput.value.trim();
+    const password = passwordInput.value.trim();
+
+    if(!email || !password) {
+        alert("Please fill in all fields.");
+        return;
+    }
+
+    const payload = {
+        email,
+        password
+    };
+
+    try {
+       setLoading(true);
+       
+       const result = await postData("/auth/login", payload);
+
+       console.log("Login response:", result);
+
+       if(result.success) {
+            localStorage.setItem("token", result.data.token);
+            window.location.href = "chat.html";
+       } else {
+            alert(result.message);
+       }
+       
+    } catch (error) {
+        console.error(error);
+        alert("Something went wrong. Please try again.");
+        
+    } finally {
+        setLoading(false);
+    }
+});
+
+hidebtn.addEventListener("click", () => {
+    if(passwordInput.typt === "password") {
+        passwordInput.typt = "text";
+    } else {
+        passwordInput.type = "password";
+    }
+
+    eyeOpen.classList.toggle("hidden");
+    eyeClosed.classList.toggle("hidden");
+})
