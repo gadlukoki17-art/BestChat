f173a0826b04f76a5f76b5988556962249414ea2
Gad lukoki <gadlukoki17@gmail.com>
2026-07-03 15:16:59 +0100

    Updating storage.js file

--- assets/js/modules/storage.js
+++ assets/js/modules/storage.js
@@ -1,11 +1,18 @@
-export function saveToken(token) {
-    localStorage.setItem("token", token);
+export function saveToken(token, remember) {
+    if(remember) { 
+        localStorage.setItem("token", token);
+    } else {
+        sessionStorage.removeItem("token", token);
+    }    
 }
 
 export function getToken() {
-    return localStorage.setItem.getToken("token");
+    return (localStorage.setItem("token", token) ||
+        sessionStorage.removeItem("token", token)
+    );    
 }
 
 export function removeToken() {
     localStorage.removeItem("token");
+    sessionStorage.removeItem("token");
 }
