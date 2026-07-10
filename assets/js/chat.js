7d19b7ebf3619948f0c0572d07edc1b8a0c8754b
Gad lukoki <gadlukoki17@gmail.com>
2026-07-10 14:38:09 +0100

    chat.js: Implement emoji button functionality

--- assets/js/chat.js
+++ assets/js/chat.js
@@ -2,8 +2,6 @@ import { getToken } from './modules/storage.js';
 import { getData, postData } from "./modules/api.js";
 
 const chatBtn = document.getElementById("chat-btn");
-const callBtn = document.getElementById("call-btn");
-const personBtn = document.getElementById("person-btn");
 const uploatBtn = document.getElementById("uploat-btn");
 const settingBtn = document.getElementById("setting-btn");
 const newChatBtn = document.getElementById("new-chat-btn");
@@ -19,13 +17,18 @@ const conversationList = document.getElementById("conversation-list");
 const emptyConversations = document.getElementById("empty-conversations");
 const iconEdit = document.getElementById("icon-edit");
 const iconBack = document.getElementById("icon-back");
+const emojiPicker = document.getElementById("emoji-picker");
+const emojiItems = document.querySelectorAll(".emoji-item");
+const emojiBtn = document.getElementById("emoji-btn");
+const callBtn = document.getElementById("call-btn");
+const personBtn = document.getElementById("person-btn");
 const callBtn2 = document.getElementById("call-btn2");
 const videoBtn = document.getElementById("video-btn");
 const menuBtn = document.getElementById("menu-btn");
 const messagesContainer = document.getElementById("messages-container");
 const messageForm = document.getElementById("message-form");
 const addBtn = document.getElementById("add-btn");
-const emojiBtn = document.getElementById("emoji-btn");
+
 const messageInput = document.getElementById("message-input");
 
 // All variable
@@ -175,6 +178,9 @@ messageForm.addEventListener("submit", async (event) => {
 
     if(result.success) {
         messageInput.value = "";
+
+        emojiPicker.classList.add("hidden");
+
         await loadMessages(activeConversationId, true);
         await loadConversations();
     }
@@ -400,3 +406,15 @@ setInterval(() => {
 
 //newChat button
 newChatBtn.addEventListener("click", showUsers);
+
+//emoji button
+emojiBtn.addEventListener("click", () => {
+    emojiPicker.classList.toggle("hidden");
+});
+
+emojiItems.forEach((emojiButton) => {
+    emojiButton.addEventListener("click", () => {
+         messageInput.value += emojiButton.textContent;
+        messageInput.focus();
+    });
+});
