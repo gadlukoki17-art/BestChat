e6cb24379e1187dd611bbfe38cbedb21f82bb805
Gad lukoki <gadlukoki17@gmail.com>
2026-07-22 23:53:30 +0100

    Update frontend configuration

--- BestChat-server/server.js
+++ BestChat-server/server.js
@@ -15,7 +15,8 @@ app.use(
     cors({
         origin: [
             "http://127.0.0.1:5500",
-            "http://localhost:5500"
+            "http://localhost:5500",
+            "https://gadlukoki17-art.github.io"
         ]
     })
 );
